0d06e488f582f95c55b38b6df435b40a1c600423
Laura Schulz <lrs8810@gmail.com>
2019-10-20 11:31:34 -0600

    Add specs for instructor and student model

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Student do
+  describe 'attributes' do
+    it 'has a name, cohort number, and age' do
+      student = create(:student)
+
+      expect(student.name).to eq('Laura')
+      expect(student.cohort_num).to eq('1908')
+      expect(student.age).to eq(31)
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :cohort_name }
+    it { should validate_presence_of :age }
+  end
+
+  describe 'relationships' do
+    it { should have_many :instructor_students }
+    it { should have_many(:instructors).through(:instructor_students) }
+  end
+end
