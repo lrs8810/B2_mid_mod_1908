5211ef2c5535e15dbcb172bea22a67f50b885f0f
Laura Schulz <lrs8810@gmail.com>
2019-10-20 21:33:24 -0600

    Add instructor show specs

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -20,38 +20,46 @@ RSpec.describe 'As a visitor' do
         expect(page).to have_content(student_2.name)
         expect(page).to have_content('Cohort: 1910')
         expect(student_2.instructors.count).to eq(2)
-        expect(page).to have_link(student_2.instructors.pluck(:name).first)
+        expect(page).to have_content(student_2.instructors.pluck(:name).first)
       end
 
       within "#student-#{student_3.id}" do
         expect(page).to have_content(student_3.name)
         expect(page).to have_content('Cohort: 1906')
         expect(student_3.instructors.count).to eq(4)
-        expect(page).to have_link(student_3.instructors.pluck(:name).first)
+        expect(page).to have_content(student_3.instructors.pluck(:name).first)
       end
     end
 
-    # it 'I see each instructors name as a link' do
-    #   student_1 = create(:student, :with_instructors, instructor_count: 3)
-    #   student_2 = create(:student, :with_instructors, cohort: '1910', instructor_count: 2)
-    #   student_3 = create(:student, :with_instructors, cohort: '1906', instructor_count: 4)
-    #
-    #   visit '/students'
-    #
-    #   within "#student-#{student_1.id}" do
-    #     expect(page).to have_content(student_1.instructors.pluck(:name).first)
-    #   end
-    #
-    #   within "#student-#{student_2.id}" do
-    #     expect(page).to have_link(student_2.instructors.pluck(:name).first)
-    #   end
-    #
-    #   within "#student-#{student_3.id}" do
-    #     expect(page).to have_content(student_3.name)
-    #     expect(page).to have_content('Cohort: 1906')
-    #     expect(student_3.instructors.count).to eq(4)
-    #     expect(page).to have_link(student_3.instructors.pluck(:name).first)
-    #   end
+    it 'I see each instructors name as a link and upon clicking I am taken to that instructors show page' do
+      student_1 = create(:student, :with_instructors, instructor_count: 3)
+      student_2 = create(:student, :with_instructors, cohort: '1910', instructor_count: 2)
+      student_3 = create(:student, :with_instructors, cohort: '1906', instructor_count: 4)
+      instructor_1 = student_1.instructors.pluck(:name).first
+
+      visit '/students'
+
+      within "#student-#{student_1.id}" do
+        expect(page).to have_link(student_1.instructors.pluck(:name)[0])
+        expect(page).to have_link(student_1.instructors.pluck(:name)[1])
+        expect(page).to have_link(student_1.instructors.pluck(:name)[2])
+      end
+
+      within "#student-#{student_2.id}" do
+        expect(page).to have_link(student_2.instructors.pluck(:name)[0])
+        expect(page).to have_link(student_2.instructors.pluck(:name)[1])
+      end
+    
+      within "#student-#{student_3.id}" do
+        expect(page).to have_link(student_3.instructors.pluck(:name)[0])
+        expect(page).to have_link(student_3.instructors.pluck(:name)[1])
+        expect(page).to have_link(student_3.instructors.pluck(:name)[2])
+        expect(page).to have_link(student_3.instructors.pluck(:name)[3])
+      end
+
+      click_on "#{instructor_1}"
+
+      expect(current_path).to eq("/instructors/#{(student_1.instructors.first).id}")
     end
   end
 end
