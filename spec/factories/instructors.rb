64e80c07e57bc7ecd6fa2f81d089a4df094896a1
Laura Schulz <lrs8810@gmail.com>
2019-10-20 21:34:39 -0600

    Update instructors factories with student traits

--- spec/factories/instructors.rb
+++ spec/factories/instructors.rb
@@ -2,4 +2,14 @@ FactoryBot.define do
   factory :instructor do
     sequence(:name) {|i| "Mike#{i}" }
   end
+
+    trait :with_students do
+      transient do
+        student_count { 3 }
+      end
+
+    after(:create) do |instructor, evaluator|
+      instructor.students << create_list(:student, evaluator.student_count)
+    end
+  end
 end
